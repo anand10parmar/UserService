1d97d4a97377aaa291853a22299747c425815a92
anand10parmar <anand10parmar@gmail.com>
2018-11-15 12:57:54 -0500

    first commit

---
+++ src/main/java/com/restAPI/myNewApp/UserController.java
@@ -0,0 +1,38 @@
+package com.restAPI.myNewApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+
+
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping(value="/hello")
+	public String helloMapping() {
+		return "Hello ! Welcome to Mintex User Service";
+	}
+	
+	@GetMapping(value ="/allusers")
+	public List<User> getAllUsers(){
+		return userService.findAll();
+	}
+	
+	@GetMapping(value = "/getuser/{username}")
+	public User getAllUsers(@PathVariable("username") String usernameInput) {
+		System.out.println(usernameInput);
+		return userService.getByUsername(usernameInput);
+	}
+	
+	@PostMapping(value = "/addnewuser")
+	public User adduser(@RequestBody User newUser) {
+		return userService.save(newUser);
+	}
+}
---
+++ src/main/java/com/restAPI/myNewApp/UserService.java
@@ -0,0 +1,16 @@
+package com.restAPI.myNewApp;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+
+public interface UserService extends JpaRepository<User, Integer> {
+
+	@Query(name="getAllUsers", value = "select u from User u")
+	public List<User> getAllUsers();
+	
+	public User getByUsername(String username);
+}
