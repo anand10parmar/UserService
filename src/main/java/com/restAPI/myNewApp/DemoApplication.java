da3462a512983d91ce65068e500ebc4c1bf4db21
anand10parmar <anand10parmar@gmail.com>
2018-11-16 10:50:57 -0500

    First commit for userservice

--- src/main/java/com/restAPI/myNewApp/DemoApplication.java
+++ src/main/java/com/restAPI/myNewApp/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.restAPI"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
